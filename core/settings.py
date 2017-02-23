6cafaa329475c7e136eca096b9d78b68a0b3b9dc
Julian Ohta <julianohta@gmail.com>
2017-02-23 13:58:08 -0500

    added static files as submodule to django-backend

--- core/settings.py
+++ core/settings.py
@@ -61,7 +61,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            'templates'
+            'static/templates'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
