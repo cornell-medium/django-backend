c85f782df4df76f606f94f5db8d1db83a87d74f5
fawnwong <fawn.wong@live.com>
2017-08-10 17:56:38 -0400

    minor changes

--- core/settings.py
+++ core/settings.py
@@ -108,7 +108,7 @@ USE_L10N = True
 USE_TZ = True
 
 # File uploads
-FILE_UPLOAD_PERMISSIONS = 0644
+FILE_UPLOAD_PERMISSIONS = 0o644
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
