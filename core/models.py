e27a4ec06b9a0142152f60a0186b084866e345a9
Brian Guo <brianguosh@gmail.com>
2017-10-29 16:19:59 -0400

    made articles model maybe this will help

--- core/models.py
+++ core/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 
 class Event(models.Model):
-	title = models.CharField(max_length=20)
-	image = models.ImageField("Cover (best ratio is 3:2)", upload_to='static/imgs/events')
-	location = models.CharField(blank=True, null=True, max_length = 35)
-	date = models.DateField()
-	start_time = models.TimeField(blank=True, null=True)
-	end_time = models.TimeField(blank=True, null=True)
-	description = models.TextField()
-	facebook = models.CharField("Facebook event url:", blank=True, null=True, max_length=200)
+    title = models.CharField(max_length=20)
+    image = models.ImageField("Cover (best ratio is 3:2)", upload_to='static/imgs/events')
+    location = models.CharField(blank=True, null=True, max_length = 35)
+    date = models.DateField()
+    start_time = models.TimeField(blank=True, null=True)
+    end_time = models.TimeField(blank=True, null=True)
+    description = models.TextField()
+    facebook = models.CharField("Facebook event url:", blank=True, null=True, max_length=200)
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
+
+class Article(models.Model):
+
+    title = models.CharField(max_length=20)
+    author = models.CharField(max_length=20)
+    content = models.TextField()
