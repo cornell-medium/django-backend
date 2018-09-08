8591da0b491e69827cc169993273591a984043fc
Kelly Fam <kellyfam@kellys-mbp.home>
2018-09-08 17:56:03 -0400

    contact page

--- core/urls.py
+++ core/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     url(r'^joinus/$', views.joinus, name='joinus'),
     url(r'^raw/$', views.rawexpo, name='rawexpo'),
     url(r'^teams/$', views.teams, name='teams'),
+    url(r'^contact/$', views.contact, name='contact'),
     url(r'^admin/', admin.site.urls),
 ]
--- core/views.py
+++ core/views.py
@@ -59,6 +59,10 @@ def joinus(request):
     context = {}
     return render(request, 'sections/joinus.html', context)
 
+def contact(request):
+    context = {}
+    return render(request, 'sections/contact.html', context)
+
 
 def rawexpo(request):
     context = {}
