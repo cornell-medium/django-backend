6e50370c03125a67af0657ab29171b6d7dc6c634
nst45 <nst45@cornell.edu>
2019-03-03 17:38:31 -0500

    Update model2

--- core/models2/Profile.py
+++ core/models2/Profile.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Profile(models.Model):
-    name = models.CharField(max_length=70)
     bio = models.TextField()
     year = models.CharField(max_length = 10)
     major = models.CharField(max_length=100) #some sort of enum thingy?
@@ -9,15 +8,8 @@ class Profile(models.Model):
     grad_date = models.DateTimeField(blank=True, null=True)
     link = models.CharField(max_length=100)
 
-    # contributor = ForeignKey(to=Contributor, related_name="contributors", null=True, blank=True)
 
 def __str__(self):
         return self.name
 
-
-# class Contributor(models.Model):
-#     project = 
-#     profile = 
-#     owned_proj =
-#     name =  #sync with Profile? 
 
