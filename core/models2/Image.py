89ba648887d9758bb8546434f996f0d065634298
nst45 <nst45@cornell.edu>
2019-03-23 14:18:56 -0400

    update models2

--- core/models2/Image.py
+++ core/models2/Image.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Image(models.Model):
     name = models.CharField(max_length=70)
+    
     filepath = models.CharField(max_length=150)
     caption = models.TextField()
 
